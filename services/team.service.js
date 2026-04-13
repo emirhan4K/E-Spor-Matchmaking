7f64daf815fbbaa377dec54804d598d460347e6c
Emirhan <emirhan.bda0@gmail.com>
2026-04-13 11:23:36 +0300

    DF

--- services/team.service.js
+++ services/team.service.js
@@ -1,11 +1,32 @@
 const teamRepository = require("../repositories/team.repository");
 const userRepository = require("../repositories/user.repository");
 
-class TeamService{
-    async createTeam(teamId,name){
-        const team = await userRepository.findById(teamId);
-        if(team || team !== null){
-            throw new Error("Zaten bir takımdasın")
-        }
+class TeamService {
+  async createTeam(userId, teamName) {
+    const user = await userRepository.findById(userId);
+    if (!user) {
+      throw new Error("Kullanıcı bulunamadı!");
     }
+    if (user.team) {
+      throw new Error("Zaten bir takımdasın!");
+    }
+    const newTeam = await teamRepository.createTeam({
+      name: teamName,
+      leader: userId,
+      members: [userId],
+    });
+    await userRepository.updateProfile(userId,{
+        team:newTeam.id
+    })
+    return newTeam;
+  }
+  async getTeamById(teamId) {
+    const getAll = await teamRepository.getTeamById(teamId);
+    if (!getAll) {
+      throw new Error("Takım bulunamadı!");
+    }
+    return getAll;
+  }
 }
+
+module.exports = new TeamService();
