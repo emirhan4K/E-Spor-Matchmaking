fa9a77532aa4a9629fc508ecf21fa774007f0f11
Emirhan <emirhan.bda0@gmail.com>
2026-04-13 10:23:31 +0300

    d

--- services/queue.service.js
+++ services/queue.service.js
@@ -2,23 +2,25 @@ class QueueService {
   constructor() {
     this.waitingPlayers = []; 
   }
-
   joinQueue(player) {
     if(!player){
         throw new Error("Oyuncu bulunamadı!")
     }
-    const players = this.waitingPlayers.find((p)=> p.socketId)
-    if(!player){
-        this.waitingPlayers.push(players)
+    const exists  = this.waitingPlayers.find((p)=> p.socketId === player.socketId)
+    if(!exists ){
+        this.waitingPlayers.push(player)
     }
   }
-
   leaveQueue(socketId) {
-   
+    this.waitingPlayers = this.waitingPlayers.filter((player)=> player.socketId !== socketId);
   }
 
   checkMatch() {
-
+    if(this.waitingPlayers.length >= 2){
+     const matchedPlayers = this.waitingPlayers.splice(0,2);
+     return matchedPlayers;
+    }
+    return null;
   }
 }
 
