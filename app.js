392691568ee8e2e9127f946ba5d2fdc7a86707b5
Emirhan <emirhan.bda0@gmail.com>
2026-04-14 14:07:30 +0300

    S

--- app.js
+++ app.js
@@ -3,16 +3,21 @@ dotenv.config();
 
 const express = require("express");
 const http = require("http");
-const socket = require("./utils/socket"); 
-const connectDB = require("./config/db"); 
 const path = require("path");
 
+//Utils & DB
+const socket = require("./utils/socket"); 
+const connectDB = require("./config/db"); 
+
+//PROJEYİ BAŞLATMA
 const app = express();
 connectDB();
+
+//Middlewares
 app.use(express.json());
-//Middleware
 const errorHandler = require("./middlewares/errorHandler.middleware");
 
+//Route Imports
 const authRoutes = require("./routes/auth.routes");
 const userRoutes = require("./routes/user.routes");
 const statRoutes = require("./routes/stat.routes");
@@ -20,6 +25,7 @@ const teamRoutes = require("./routes/team.routes");
 const matchRoutes = require("./routes/match.routes");
 const walletRoutes = require("./routes/wallet.routes");
 
+//API Routes
 app.use("/api/auth",authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/stats",statRoutes)
@@ -28,10 +34,13 @@ app.use("/api/matches", matchRoutes);
 app.use("/api/wallet", walletRoutes);
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-app.use(errorHandler); //Global hata yakalayıcı
+//Global hata yakalayıcı
+app.use(errorHandler); 
+
 const httpServer = http.createServer(app);
 const io = socket.init(httpServer);
 
+// Canlı Eşleştirme Kuyruğunu Başlat
 require("./sockets/queue.socket")(io);
 
 httpServer.listen(3000, () => {
