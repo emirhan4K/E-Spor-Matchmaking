a0fdce2b9368c27b0970eb9bdc7a313adfd3df77
Emirhan <emirhan.bda0@gmail.com>
2026-04-14 14:49:02 +0300

    s

--- controllers/friendship.controller.js
+++ controllers/friendship.controller.js
@@ -3,9 +3,9 @@ const friendshipService = require("../services/friendship.service");
 class FriendshipController{
     async sendRequest(req,res,next){
         const userId = req.user.userId;
-        const response = req.params.id;
+        const recipientId = req.params.id;
         try {
-            const result = await friendshipService.sendRequest(userId,response);
+            const result = await friendshipService.sendRequest(userId,recipientId);
             res.status(201).json({message:"Arkadaşlık isteği gönderdildi",result})
         } catch (error) {
             next(error)
