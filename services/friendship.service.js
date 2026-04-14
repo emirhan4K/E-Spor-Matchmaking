4a5368bc2b450276f82f7a94239b25abe0c6c2a6
Emirhan <emirhan.bda0@gmail.com>
2026-04-14 14:39:03 +0300

    s

--- services/friendship.service.js
+++ services/friendship.service.js
@@ -6,10 +6,16 @@ class FriendshipService{
         if(requesterId === recipientId){
             throw new Error("Kendinize arkadaşlık isteği gönderemezsiniz.");
         }
-        const user = await userRepository(requesterId,recipientId);
+        const user = await userRepository.findById(recipientId); //İstek atılan kullanıcı var mı ?
         if(!user){
             throw new Error("Kullanıcı bulunamadı!")
         }
-        
+        const control = await friendshipRepository.checkExistingRequest(requesterId,recipientId)
+        if(control){
+            throw new Error("Zaten bir istek var veya arkadaşsınız");
+        }
+        const saved = await friendshipRepository.createRequest(requesterId,recipientId);
+        return saved;
     }
 }
+module.exports = new FriendshipService();
